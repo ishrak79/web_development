b309c8fcaff32b95825c7e41308ab5c2f4618cfc
ishrak79 <rumaiyasultana2@gmail.com>
2025-12-12 22:07:31 +0600

    js

---
+++ java script/java script intro/script.js
@@ -0,0 +1,30 @@
+//variable: a location/storage/place in computer's memory where we can store values/data and give it a name
+//syntax : The writing rules of any programing language
+//syntax of writing a variable : let/const variable_name = value/data;
+let num1 = 67;
+document.writeln(num1)
+document.writeln(`<br>`)
+document.writeln(typeof num1);
+document.writeln(`<br>`)
+document.writeln(`<br>`)
+
+let num2 = 67.67;
+document.writeln(num2)
+document.writeln(`<br>`)
+document.writeln(typeof num2);
+document.writeln(`<br>`)
+document.writeln(`<br>`)
+
+let my_string = "I am string 1213 ^$&*O*";
+document.writeln(my_string)
+document.writeln(`<br>`)
+document.writeln(typeof my_string);
+document.writeln(`<br>`)
+document.writeln(`<br>`)
+
+let my_boolen = true;
+document.writeln(my_boolen)
+document.writeln(`<br>`)
+document.writeln(typeof my_boolen);
+document.writeln(`<br>`)
+document.writeln(`<br>`)
