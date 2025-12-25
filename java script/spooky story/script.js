a723cc004abee9d48e90bb78b5fac58b722fa155
ishrak79 <rumaiyasultana2@gmail.com>
2025-12-25 20:54:34 +0600

    html

---
+++ java script/spooky story/script.js
@@ -0,0 +1,12 @@
+
+function typeAndScream() {
+    let personName = document.getElementById
+    ("name").value;
+    alert("i told you not to do that" + personName);
+}
+function goDark() {
+    document.body.className = "dark";
+}
+function finishTheStory() {
+    document.getElementById("story").innerHTML="So I carefully searced the damp dungeon. There you are!";
+}
