4bca119de43334aac1d5d636fb239260fe0abc5b
ishrak79 <rumaiyasultana2@gmail.com>
2025-12-26 19:25:47 +0600

    hw

---
+++ java script/spooky story hw/script.js
@@ -0,0 +1,12 @@
+
+function typeAndScream() {
+    let personName = document.getElementById
+    ("name").value;
+    alert("I think it is gardening too" + personName);
+}
+function goDark() {
+    document.body.className = "dark";
+}
+function finishTheStory() {
+    document.getElementById("story").innerHTML="Gardening is the practice of growing plants, flowers, fruits, or vegetables in soil.Plants in a garden need sunlight, water, and care to grow well.Gardening also helps the environment by providing oxygen and food for insects.Many people enjoy gardening because it is both fun and healthy.";
+}
